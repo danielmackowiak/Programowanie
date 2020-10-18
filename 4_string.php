65b6988d19bfaf4248d64f88752f5a90ed19db78
danielmackowiak <daniel.mckwk@gmail.com>
2020-10-18 08:31:25 +0200

    php

--- 4_string.php
+++ 4_string.php
@@ -51,5 +51,29 @@ LOREM;
     $address = 'Poznań, ul. Rynek Jeżycki 1, tel. (61) 123-45-67';
     echo strstr($address, 'tel');
 
+    $address = 'Poznań, ul. Rynek Jeżycki 1, tel. (61) 123-45-67';
+    echo strstr($address, 'tel' true), '<br>';
+
+    $address = 'Poznań, ul. Rynek Jeżycki 1, tel. (61) 123-45-67';
+    echo strstr($address, 'Tel'), '<br>';
+
+    $mail = strstr('janusz@gmail.com', "@");
+    echo "$mail<br>";
+
+    $mail = strstr('janusz@gmail.com', '64');
+    echo "$mail<br>";
+
+// substr
+    echo substr('Janusz Kowalski', 7, 5);
+    echo strlen(substr('Janusz Kowalski', 7, 5));
+    ob_clean();
+
+// zmiana ciągów
+    $replace = str_replace('%korona%','grypa', '%korona% jest niebezpieczna');
+    echo $replace,'<br>';
+
+// usuń z loginu polskie znaki
+    $login = 'bączek';
+    $censure = array('ą', 'ę', ...)
 
 ?>

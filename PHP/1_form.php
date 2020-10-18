94ebc5259d1ea5514b50ff6442acbef91f8b5ab3
danielmackowiak <daniel.mckwk@gmail.com>
2020-10-18 10:56:28 +0200

    18/10

--- PHP/1_form.php
+++ PHP/1_form.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formularz z danymi użytkownika</title>
+    <title>Formularz</title>
 </head>
 <body>
     <h4>Formularz z danymi użytkownika</h4>
@@ -11,9 +11,14 @@
         <input type="text" name="name" placeholder="Imię"><br><br>
         <input type="text" name="surname" placeholder="Nazwisko"><br><br>
         <input type="password" name="pass" placeholder="Hasło"><br><br>
-        <input type="text" name="zipcode" size="2" maxlength="2" placeholder="Kod Pocztowy"><br><br>
-        <input type="text" name="zipcode2" size="2" maxlength="3" placeholder="Kod Pocztowy2"><br><br>
-        <input type="submit" value="Wyślij dane"
+        <input type="text" name="zipcode" size="1" maxlength="2" placeholder="00"> -
+        <input type="text" name="zipcode2" size="2" maxlength="3" placeholder="000"><br><br>
+        <h4>Ulubiony kolor</h4>
+        <input type="radio" name="color"value="r" checked>Czerwony
+        <input type="radio" name="color"value="g">Zielony
+        <input type="radio" name="color"value="b">Niebieski<br><br>
+
+        <input type="submit" value="Wyślij dane">
     </form>
 </body>
 </html>
--- PHP/1_form_data.php
+++ PHP/1_form_data.php
@@ -23,23 +23,38 @@
 
 // Sprawdź czy użytkownik wypełnił wszystkie dane, jeśli tak to wyświetl mu heredoc, w przeciwnym razie przekieruj użytkownika do formularza
 
-        if (!empty($_GET['name']) && !empty(GET['surname']) && !empty(GET['zipcode']) && !empty(GET['zipcode2'])) {
-            echo 'Dane wprowadzone poprawnie';
-            echo <<<T
-            Imię i Nazwisko: $_GET[name] $_GET[surname]<br>
-            Hasło: {$_GET['pass']}<br>
-            Kod Pocztowy1: $_GET[zipcode]-$_GET[zipcode2]
-T;
-        }else {
-            echo 'Wypełnij wszystkie dane!';
-        }
-
 // po nie wypełnieniu danych, przekierowywuje użytkownika do formularza, dane w formularzu mają byc wypełnione (oprócz hasła)
 // wykorzystaj skrypt w js (historia)
 
+// Wykorzystaj switch do wyświetlenia koloru np. czerwony
 
-
-
+        if (!empty($_GET['name']) && !empty($_GET['surname']) && !empty($_GET['pass'] && !empty($_GET['zipcode']) && !empty($_GET['zipcode2']) && !empty($_GET['color'])) {
+            $color = $_GET['color']
+            switch ($color) {
+                case 'r':
+                    &color = 'czerwony'
+                break;
+                case 'g':
+                    &color = 'zielony'
+                case 'b':
+                    &color = 'niebieski'
+                break;
+            break;
+            echo <<<T
+            Imię i Nazwisko: $_GET[name] $_GET[surname]<br>
+            Hasło: {$_GET['pass']}<br>
+            Kod Pocztowy: $_GET[zipcode]-$_GET[zipcode2]
+            Ulubiony kolor: $color
+            T;
+            }
+        }else {
+           // header('location: ./1_form.php');
+           ?>
+           <script>
+                history.back();
+           </script>
+           <?php
+        }
     ?>
 </body>
 </html>

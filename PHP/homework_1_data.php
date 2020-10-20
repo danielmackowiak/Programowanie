a176b39b10586ae2e486c62ecfcb6c65ed9ebd47
danielmackowiak <daniel.mckwk@gmail.com>
2020-10-20 20:45:17 +0200

    Zadanie domowe

--- PHP/homework_1_data.php
+++ PHP/homework_1_data.php
@@ -1,3 +1,14 @@
+<?php
+// Użytkownik podaje z klawiatury dwie długości do wyboru w polu radio ma pole i obwód domyślnie zaznaczone ma być 'pole'
+// Zabezpiecz formularz przed podaniem pustych danych
+
+// Wynik wyświetl w formacie:
+// Pole wynosi: 20cm2 (2 ma byc w indeksie górnym)
+// Obwód wynosi: 20cm
+// Dane wyślij z formularza bezpiecznie (POST)
+// Obliczenia mają być wyidoczne w innym pliku
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
